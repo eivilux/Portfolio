7afdd4fb9566973b7d9850bf1ce633a08eb982a4
enzoblain <enzoblain@proton.me>
2026-07-31 15:12:28 +0200

    fix: numerotation

--- src/lib/components/layout/Container.svelte
+++ src/lib/components/layout/Container.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import { app_container } from '$lib/app/AppContainer';
 
-	let sectionNumber = $state(0);
+	let sectionNumber = 0;
 </script>
 
 <nav>
 	{#each app_container.items() as item, index}
+		{@const currentNumber = item.numbered ? sectionNumber++ : -1}
+
 		<div
 			class="w-full {index % 2 !== 0 ? 'bg-surface' : 'bg-background'} {index !== 0
 				? 'border-t border-neutral-800'
@@ -17,7 +19,7 @@
 			>
 				{#if item.numbered}
 					<div class="pb-4 text-primary">
-						0{++sectionNumber} — {item.name}
+						{currentNumber.toString().padStart(2, '0')} — {item.name}
 					</div>
 				{/if}
 
